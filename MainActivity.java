cf9a379b8c884abf9c5c4208d4e14380e92bfeae
Atharva Bhaindarkar <86788103+atharvaproject@users.noreply.github.com>
2023-01-16 14:44:15 +0530

    Create MainActivity.java

---
+++ MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.accessblityexample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button allowPermission;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        allowPermission = findViewById(R.id.allowPermission);
+        allowPermission.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Settings.ACTION_ACCESSIBILITY_SETTINGS));
+            }
+        });
+    }
+}
